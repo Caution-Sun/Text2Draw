679e4a4aa09dd48a1fc39a17d4b82fcc342833fb
kkyh1125 <kkyh1125@naver.com>
2022-05-03 16:40:36 +0900

    fix(ImageRequester): AsyncTask 에서 RxJava 로 코드 수정

--- app/src/main/java/org/techtown/capston_sample_1/ImageRequester.java
+++ app/src/main/java/org/techtown/capston_sample_1/ImageRequester.java
@@ -2,15 +2,18 @@ package org.techtown.capston_sample_1;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 
-import java.io.BufferedInputStream;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Consumer;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class ImageRequester {
     final String serverIp;
@@ -19,7 +22,7 @@ public class ImageRequester {
     private Socket clientSocket;
     private PrintWriter pw;
 
-    private ImageAsyncTask imageAsyncTask;
+    private Disposable disposable;
     private ImageCallback imageCallback;
 
     private Bitmap resultBitmap;
@@ -30,22 +33,19 @@ public class ImageRequester {
         this.imageCallback = imageCallback;
     }
 
-    public void requestImage(String translatedInput, String styleInput) {
-        imageAsyncTask = new ImageAsyncTask();
-        imageAsyncTask.execute(translatedInput, styleInput);
+    public void requestImage(String translatedInput, String styleInput, String quality) {
+        execute(translatedInput, styleInput, quality);
     }
 
-    private class ImageAsyncTask extends AsyncTask<String, Void, Bitmap> {
-
-        @Override
-        protected Bitmap doInBackground(String... strings) {
+    private void execute(String translatedInput, String styleInput, String quality) {
+        disposable = Observable.fromCallable(() -> {
             try {
                 clientSocket = new Socket();
                 clientSocket.connect(new InetSocketAddress(serverIp, serverPort), 5 * 1000);
 
                 // 데이터 전송
                 pw = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream(), "UTF-8"));
-                pw.write(strings[0] + " | " + strings[1]);
+                pw.write(translatedInput + "|" + styleInput + "|" + quality);
                 pw.flush();
 
                 // 데이터 수신
@@ -65,12 +65,12 @@ public class ImageRequester {
             }
 
             return resultBitmap;
-        }
-
-        @Override
-        protected void onPostExecute(Bitmap bitmap) {
-            imageCallback.onResult(bitmap);
-        }
+        }).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<Bitmap>() {
+            @Override
+            public void accept(Bitmap bitmap) throws Throwable {
+                imageCallback.onResult(bitmap);
+            }
+        });
     }
 
 }
